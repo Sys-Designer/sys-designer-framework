b31ca501966b876005d7f5a42fa5f02c42e5ac2d
Jia wang Qin <1354174190qjw@sina.com>
2026-06-28 08:27:12 +0800

    替换druid starter

--- sys-designer-framework-spring-boot/src/main/java/com/sys/designer/framework/common/config/DatasourceWrapper.java
+++ sys-designer-framework-spring-boot/src/main/java/com/sys/designer/framework/common/config/DatasourceWrapper.java
@@ -1,11 +1,11 @@
 package com.sys.designer.framework.common.config;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceWrapper;
+import com.alibaba.druid.pool.DruidDataSource;
 import com.sys.designer.framework.api.security.Rsa2CryptoService;
 import com.sys.designer.framework.common.util.ComponentUtil;
 import com.sys.designer.framework.common.util.ValueUtil;
 
-public class DatasourceWrapper extends DruidDataSourceWrapper {
+public class DatasourceWrapper extends DruidDataSource {
     private String datasourceId;
 
     public DatasourceWrapper(String datasourceId) {
